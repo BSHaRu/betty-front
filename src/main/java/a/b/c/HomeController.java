9cf5617836daec09a4334ace2d336dbbc01e1ae0
test_git <suj0309@naver.com>
2022-08-14 16:05:24 +0900

    최초 설정

---
+++ src/main/java/a/b/c/HomeController.java
@@ -0,0 +1,71 @@
+package a.b.c;
+
+import java.text.DateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ * Handles requests for the application home page.
+ */
+@Controller
+public class HomeController {
+	
+	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
+	
+	/**
+	 * Simply selects the home view to render by returning its name.
+	 */
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	public String home(Locale locale, Model model) {
+		logger.info("Welcome home! The client locale is {}.", locale);
+		
+		Date date = new Date();
+		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
+		
+		String formattedDate = dateFormat.format(date);
+		
+		model.addAttribute("serverTime", formattedDate );
+		
+		return "home";
+	}
+	
+	@GetMapping("/bookDetails")
+	public String a() {
+		return "/book/bookDetails";
+	}
+		@GetMapping("/bookList")
+	public String aa() {
+		return "/book/bookList";
+	}
+	
+	@GetMapping("/login")
+	public String c() {
+		return "/member/login";
+	}
+	@GetMapping("/signUP")
+	public String cc() {
+		return "/member/signUp";
+	}
+	
+	@GetMapping("/myPage")
+	public String d() {
+		return "/myPage/myPage";
+	}
+	@GetMapping("/rentalBook")
+	public String dd() {
+		return "/myPage/rentalBook";
+	}
+	@GetMapping("/userInfo")
+	public String ff() {
+		return "/manager/userInfo";
+	}
+	
+}
